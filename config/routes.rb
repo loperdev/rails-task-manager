f67fba90abe90741315c6364ae1c1c5e7184c0dd
Dev Vyaas <devendravyas04@gmail.com>
2021-11-11 16:56:08 +0530

    index page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # CRUD
+  # # CREATE, READ, UPDATE, DELETE
+  # # verb 'path', to: controller#action
+  # # read all the tasks
+  # get '/tasks', to: 'tasks#index'
+  # # read one task
+  # get '/tasks/:id', to 'tasks#show'
+  # # : means this value here is dynamic which will change
+  # # create a task
+  # get '/tasks/new', to 'tasks#new' # display the form
+  # post '/tasks', to: 'tasks#create'
+  # # update a task
+  # get '/tasks/:id/edit', to: 'tasks#edit'
+  # patch '/tasks/:id', to: 'task#update'
+  # # delete a task
+  # delete '/tasks/:id', to: 'task#destroy'
+  resources :tasks
 end
