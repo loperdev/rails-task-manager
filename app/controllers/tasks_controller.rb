fe69c7c576cd3cbf1edec3094d14b826befde2ce
Dev Vyaas <devendravyas04@gmail.com>
2021-11-11 20:09:43 +0530

    finally completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,43 @@
 class TasksController < ApplicationController
- def index
-  @tasks = Task.all
- end
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
 
- def show
- end
+  def index
+    @tasks = Task.all
+  end
 
- def create
- end
+  def show
+  end
 
- def edit
- end
+  def new
+    @task = Task.new
+  end
 
- def update
- end
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
+  end
 
- def destroy
- end
+  def edit
+  end
 
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,2 @@
 class Task < ApplicationRecord
-  validates :title, presence: true
-  validates :details, presence: true
-  validates :completed, presence: true
 end
